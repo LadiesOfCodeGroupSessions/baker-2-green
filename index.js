37d735406cc70177d658c330b6fb33b3794822e4
Emma <48389529+emjogale@users.noreply.github.com>
2023-01-18 18:34:22 +0000

    start index.js and export module

---
+++ index.js
@@ -0,0 +1,5 @@
+const bakery = {
+
+};
+
+module.exports = bakery;
--- index.test.js
+++ index.test.js
@@ -1,7 +1,13 @@
 const bakery = require("/.index.js");
 
 describe("index", () => {
-   it("cakes returns max number of cakes", () => {
+  it("recipe1 returns 2", () => {
+     const recipe1 = { flour: 500, sugar: 200, eggs: 1 };
+     const ingredients = { flour: 1200, sugar: 1200, eggs: 5, milk: 200 };
+     
+     let maxCakes = (bakery.cakes(recipe1, ingredients));
 
-   }) ;
+     expect(maxCakes).toBe(2);
+       
+   }) 
 });
